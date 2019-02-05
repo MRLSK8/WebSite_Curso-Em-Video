542eaea082214b743b743e31975536231fd859f6
Marcelo Lima <marcelo_sp5@hotmail.com>
2019-02-05 11:07:56 -0300

    Finalizado

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -1,3 +1,8 @@
 function mudaFoto(foto) {
     document.getElementById('icone').src = foto;
 }
+function calc_total() {
+    const Qtd = document.getElementById("cQtd").value;
+
+    document.getElementById("cTot").value = Qtd * 1500;
+}
